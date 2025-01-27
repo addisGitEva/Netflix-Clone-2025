8950223a22411f356ed77eae92624ae55cb4d7c6
addisGitEva <addiswendu4@gmail.com>
2025-01-27 11:00:21 +0300

    Netflix Row

--- p4-netflix-clone/src/Pages/Home/Home.js
+++ p4-netflix-clone/src/Pages/Home/Home.js
@@ -2,11 +2,13 @@ import React from "react";
 import Header from "../../Components/Header/Header";
 import Footer from "../../Components/Footer/Footer";
 import Banner from "../../Components/Banners/Banner";
+import Rowlist from "../../Components/Rows/RowList/Rowlist";
 function Home() {
   return (
     <div>
       <Header />
       <Banner />
+      <Rowlist />
       <Footer />
     </div>
   );
